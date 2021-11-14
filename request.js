7358a7f2692c783a6c2c4e54cbfacf721e420613
KiditsGood <sadol2001@mail.ru>
2021-11-14 16:02:03 +0300

    test

--- request.js
+++ request.js
@@ -4,68 +4,35 @@ function getUserInfo(nickname) {
         url: 'https://cwd-monitoring.herokuapp.com/get-acc/' + nickname,
         dataType: 'json',
         success: data => {
-            console.log(data)
             $('#info').html(data.account.login);
             $('#info1').html(data.account.id);
             $('#info2').html(data.account.cwd_balance);
-            console.log(data.operations);
-            console.log(Object.values(data.contacts));
             var contacts = '';
             Object.values(data.contacts).forEach(e=>{
                 contacts +=('<p>' + e + '</p>');
             })
-            console.log(contacts);
             $('#info6').html(contacts);
             var table = '';
             var txt = '';
             var accept = '';
             for (let i = 0; i < data.operations.length; i++) {
                 var oper = data.operations[i];
-                oper.fee.asset_id = currencyConvert(oper.fee.asset_id);
                 if (oper.operation == 'Создание ордера на обмен'){
-                    oper.sell.asset_id = currencyConvert(oper.sell.asset_id);
-                    oper.receive.asset_id = currencyConvert(oper.receive.asset_id);
-                    table +=
-                        '<tr>' +
-                        '<td>' + oper.operation + '</td>' +
-                        '<td>' + oper.fee.amount + '</td>' +
-                        '<td>' + oper.fee.asset_id +'</td>' +
-                        '<td>' + oper.sell.amount + '</td>' +
-                        '<td>' + oper.sell.asset_id + '</td>' +
-                        '<td>' + oper.receive.amount + '</td>' +
-                        '<td>' + oper.receive.asset_id + '</td>' +
-                        '</tr>';
+                    oper.sell.asset_id = currencyReplace(oper.sell.asset_id);
+                    oper.receive.asset_id = currencyReplace(oper.receive.asset_id);
+                    table += sellTable(oper);
                 }
                 else if (oper.operation == 'Перевод'){
-                    oper.amount.asset_id = currencyConvert(oper.amount.asset_id);
+                    oper.amount.asset_id = currencyReplace(oper.amount.asset_id);
                     oper.from = clientReplace(oper.from, data);
                     oper.to = clientReplace(oper.to, data);
-                    txt +=
-                        '<tr>' +
-                        '<td>' + oper.operation + '</td>' +
-                        '<td>' + oper.fee.amount + '</td>' +
-                        '<td>' + oper.fee.asset_id +'</td>' +
-                        '<td>' + oper.amount.amount + '</td>' +
-                        '<td>' + oper.amount.asset_id + '</td>' +
-                        '<td>' + oper.from + '</td>' +
-                        '<td>' + oper.to + '</td>' +
-                        '</tr>';
+                    txt += amountTable(oper);
                 }
                 else if (oper.operation == 'Подтверждение заявки на обмен'){
-                    oper.amount.asset_id = currencyConvert(oper.amount.asset_id);
+                    oper.amount.asset_id = currencyReplace(oper.amount.asset_id);
                     oper.client = clientReplace(oper.client, data);
                     oper.reciever = clientReplace(oper.reciever, data);
-                    accept +=
-                        '<tr>' +
-                        '<td>' + oper.operation + '</td>' +
-                        '<td>' + oper.fee.amount + '</td>' +
-                        '<td>' + oper.fee.asset_id +'</td>' +
-                        '<td>' + oper.amount.amount + '</td>' +
-                        '<td>' + oper.amount.asset_id + '</td>' +
-                        '<td>' + oper.price/(10 ** (8)) + '</td>' +
-                        '<td>' + oper.client + '</td>' +
-                        '<td>' + oper.reciever + '</td>' +
-                        '</tr>';
+                    accept += clientTable(oper);
                 }
             }
             $('#info3').html(table);
@@ -79,7 +46,7 @@ function formHandler(event) {
     getUserInfo(event.currentTarget.elements.nickname.value);
 }
 
-function currencyConvert(id) {
+function currencyReplace(id) {
     if (id == '1.3.0'){
         return 'CWD';
     }
@@ -95,4 +62,42 @@ function clientReplace(id, {contacts, account}) {
     }
 
     return contacts[id];
+}
+function sellTable(oper) {
+
+    return('<tr>' +
+        '<td>' + oper.operation + '</td>' +
+        '<td>' + currencyConvert(oper.sell.asset_id,oper.sell.amount) + '</td>' +
+        '<td>' + oper.sell.asset_id + '</td>' +
+        '<td>' + currencyConvert(oper.receive.asset_id,oper.receive.amount) + '</td>' +
+        '<td>' + oper.receive.asset_id + '</td>' +
+        '</tr>');
+}
+function amountTable(oper) {
+    return('<tr>' +
+        '<td>' + oper.operation + '</td>' +
+        '<td>' + currencyConvert(oper.amount.asset_id,oper.amount.amount) + '</td>' +
+        '<td>' + oper.amount.asset_id + '</td>' +
+        '<td>' + oper.from + '</td>' +
+        '<td>' + oper.to + '</td>' +
+        '</tr>');
+}
+function clientTable(oper) {
+    return('<tr>' +
+        '<td>' + oper.operation + '</td>' +
+        '<td>' + currencyConvert(oper.amount.asset_id,oper.amount.amount) + '</td>' +
+        '<td>' + oper.amount.asset_id + '</td>' +
+        '<td>' + oper.price/(10 ** (8)) + '</td>' +
+        '<td>' + oper.client + '</td>' +
+        '<td>' + oper.reciever + '</td>' +
+        '</tr>');
+}
+function currencyConvert(currency, amount) {
+    if (currency == 'CWD'){
+        return amount / (10 ** (5));
+    }
+    if (currency == 'PLEASURE'){
+        return amount / (10 ** (4));
+    }
+    return (amount);
 }
